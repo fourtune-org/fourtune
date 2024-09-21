ff42292237bf52b8651a94d71e42aaf5f6ac3b29
eXory2024 <exory@anio.software>
2024-09-21 13:25:41 +0200

    Always execute fourtune in the project's root.

--- src/index.mjs
+++ src/index.mjs
@@ -1,32 +1,41 @@
 import createFourtuneSession from "./lib/init/createFourtuneSession.mjs"
 import stages from "./stages/index.mjs"
+import process from "node:process"
 
 export default async function(project_root) {
-	const fourtune_session = await createFourtuneSession(project_root)
+	const saved_cwd = process.cwd()
 
-	if ("preinit" in fourtune_session.project.config) {
-		const {preinit} = fourtune_session.project.config
+	try {
+		process.chdir(project_root)
 
-		if (!Array.isArray(preinit)) {
-			throw new Error(
-				`preinit must be an array of functions.`
-			)
+		const fourtune_session = await createFourtuneSession(project_root)
+
+		if ("preinit" in fourtune_session.project.config) {
+			const {preinit} = fourtune_session.project.config
+
+			if (!Array.isArray(preinit)) {
+				throw new Error(
+					`preinit must be an array of functions.`
+				)
+			}
+
+			for (const fn of preinit) {
+				await fn(
+					fourtune_session.public_interface
+				)
+			}
 		}
 
-		for (const fn of preinit) {
-			await fn(
-				fourtune_session.public_interface
-			)
+		await fourtune_session.initializeTarget()
+
+		for (const stage of stages) {
+			await stage(fourtune_session)
 		}
-	}
 
-	await fourtune_session.initializeTarget()
-
-	for (const stage of stages) {
-		await stage(fourtune_session)
-	}
-
-	for (const warning of fourtune_session.project.warnings) {
-		console.log(warning.id, warning.message)
+		for (const warning of fourtune_session.project.warnings) {
+			console.log(warning.id, warning.message)
+		}
+	} finally {
+		process.chdir(saved_cwd)
 	}
 }
