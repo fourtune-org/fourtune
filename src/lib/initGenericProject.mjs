bd9520d370d75ee23094b82af333ac07ff06c86b
eXory2024 <exory@anio.software>
2024-10-08 18:43:35 +0200

    Automatically create LICENSE file. Automatically create .gitignore file if it doesn't exist yet.

--- src/lib/initGenericProject.mjs
+++ src/lib/initGenericProject.mjs
@@ -13,13 +13,63 @@ indent_size = 4\n`
 		name: ".github/CODEOWNERS", contents:
 `*       @dasdeo
 *       @eXory2024\n`
+	},
+
+	{
+		name: "LICENSE", contents:
+`MIT License
+
+Copyright (c) anio.software
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.\n`
+	},
+
+	{
+		name: ".gitignore", contents:
+`.DS_Store
+/node_modules/
+/.fourtune/
+/build/
+/dist/\n`,
+		overwrite: false
 	}
 ]
 
 export default async function(fourtune_session) {
-	for (const {name, contents} of files) {
-		await writeAtomicFile(
-			path.join(fourtune_session.getProjectRoot(), name), contents
-		)
+	for (const file of files) {
+		const {name, contents} = file
+		let overwrite = false
+
+		if (!("overwrite" in file)) {
+			overwrite = true
+		} else {
+			overwrite = file.overwrite
+		}
+
+		if (overwrite) {
+			await writeAtomicFile(
+				path.join(fourtune_session.getProjectRoot(), name), contents
+			)
+		} else {
+			fourtune_session.addWarning(
+				`overwrite`, `Not overwriting already present file "${name}".`
+			)
+		}
 	}
 }
