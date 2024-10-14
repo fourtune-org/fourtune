29ba91ac444d8dad66592063594de2231b3d49ea
eXory2024 <exory@anio.software>
2024-10-14 21:59:52 +0200

    Don't specify relative and absolute paths for auto-generated files. They all belong in the same folder "src/auto", so assume that in every section that deals with auto-generated files. Also, refactor the deletion of obsolete auto-generated files.

--- src/stages/build/createAutoGeneratedFiles.mjs
+++ src/stages/build/createAutoGeneratedFiles.mjs
@@ -2,13 +2,15 @@ import path from "node:path"
 import {writeAtomicFile} from "@anio-software/fs"
 
 export default async function(fourtune_session) {
-	for (const {relative_path, generateFileSourceCode} of fourtune_session.files_to_autogenerate) {
+	for (const {file_path, generateFileSourceCode} of fourtune_session.files_to_autogenerate) {
 		const source_code = await generateFileSourceCode()
 
 		await writeAtomicFile(
-			path.join(fourtune_session.project.root, relative_path), source_code, {
+			path.join(fourtune_session.project.root, "src", "auto", file_path), source_code, {
 				create_parents: true
 			}
 		)
+
+		console.log(`\\---> wrote auto file at src/auto/${file_path}`)
 	}
 }
