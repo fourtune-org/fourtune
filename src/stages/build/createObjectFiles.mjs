c803c56b1bbcd38132a8c66998c5d4aa92e6d57d
eXory2024 <exory@anio.software>
2024-10-14 22:22:54 +0200

    Remove entries. (they are available with .getProjectSourceFiles())

--- src/stages/build/createObjectFiles.mjs
+++ src/stages/build/createObjectFiles.mjs
@@ -1,26 +1,17 @@
-import {scandir} from "@anio-software/fs"
 import path from "node:path"
 
 export default async function(fourtune_session) {
 	const project_root = fourtune_session.project.root
 
-	const entries = await scandir(
-		path.join(project_root, "build", "src"), {
-			filter({type}) {
-				return type === "file"
-			}
-		}
-	)
-
 	//
 	// realm could invoke ts here to
 	// create the definitions for all files requested
 	//
-	await fourtune_session.runTargetHooks("objects.pre", [entries])
+	await fourtune_session.runTargetHooks("objects.pre")
 
 	for (const entry of entries) {
 		//console.log(entry)
 	}
 
-	await fourtune_session.runTargetHooks("objects.post", [entries])
+	await fourtune_session.runTargetHooks("objects.post")
 }
