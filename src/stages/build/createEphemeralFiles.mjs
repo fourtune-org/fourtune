bfba43d2a35557f7bb7f2d1a9d24c4b37dcf0e49
eXory2024 <exory@anio.software>
2024-10-18 16:39:10 +0200

    - allow generator_args to be missing. - allow an ephemeral file generator to create multiple files.

--- src/stages/build/createEphemeralFiles.mjs
+++ src/stages/build/createEphemeralFiles.mjs
@@ -4,17 +4,28 @@ import {writeAtomicFile} from "@anio-software/fs"
 export default async function(fourtune_session) {
 	await fourtune_session.runTargetHooks("ephemerals.pre")
 
-	for (const {name, generator, generator_args} of fourtune_session.ephemerals) {
-		const source_code = await generator(
-			fourtune_session.public_interface,
-			...generator_args
-		)
+	for (const entry of fourtune_session.ephemerals) {
+		const ephemerals = Array.isArray(entry) ? entry : [entry]
 
-		await writeAtomicFile(
-			path.join(fourtune_session.project.root, "objects", "ephemerals", name), source_code, {
-				create_parents: true
+		for (const ephemeral of ephemerals) {
+			const {name, generator} = ephemeral
+			let generator_args = []
+
+			if ("generator_args" in ephemeral) {
+				generator_args = ephemeral.generator_args
 			}
-		)
+
+			const source_code = await generator(
+				fourtune_session.public_interface,
+				...generator_args
+			)
+
+			await writeAtomicFile(
+				path.join(fourtune_session.project.root, "objects", "ephemerals", name), source_code, {
+					create_parents: true
+				}
+			)
+		}
 	}
 
 	await fourtune_session.runTargetHooks("ephemerals.post")
