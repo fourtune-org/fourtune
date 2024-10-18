5c619064b273fcf43dd966151c95a519e877cabb
eXory2024 <exory@anio.software>
2024-10-18 16:05:55 +0200

    Pass fourtune_session.

--- src/stages/build/createEphemeralFiles.mjs
+++ src/stages/build/createEphemeralFiles.mjs
@@ -5,7 +5,10 @@ export default async function(fourtune_session) {
 	await fourtune_session.runTargetHooks("ephemerals.pre")
 
 	for (const {name, generator, generator_args} of fourtune_session.ephemerals) {
-		const source_code = await generator(...generator_args)
+		const source_code = await generator(
+			fourtune_session.public_interface,
+			...generator_args
+		)
 
 		await writeAtomicFile(
 			path.join(fourtune_session.project.root, "objects", "ephemerals", name), source_code, {
