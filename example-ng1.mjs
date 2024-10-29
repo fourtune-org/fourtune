8d88dc3b9fe44b70a191ae6a4f220c7f99732929
eXory2024 <exory@anio.software>
2024-10-29 20:13:38 +0100

    Update example-ng1.mjs.

--- example-ng1.mjs
+++ example-ng1.mjs
@@ -40,3 +40,114 @@ export const c2 = [
 ]
 //---v
 
+
+
+/*
+In fourtune you have:
+A Project with multiple or one TARGET
+
+A Target with multiple or one PRODUCT
+
+A Product with multiple or one DISTRIBUTABLE
+
+
+src/export/async/MyExport.mts
+src/export/sync/MyExportSync.mts
+
+to skip: --skip-distributable-type "types"
+to skip: --skip-distributable-types "types,source"
+
+to skip: --skip-product "async"
+to skip: --skip-products "async,sync"
+
+[to skip: --skip-targets "realm-js/target/package"]
+
+target: realm-js/target/package
+
+		product: async
+			distributable types:
+							bundle,
+							bundle.min,
+							source,
+							source.min,
+							types
+
+			distributable: index.mjs
+			distributable: index.min.mjs
+			distributable: source.min.mjs
+			distributable: index.d.mts
+
+		product: sync
+			distributable: index.mjs
+			distributable: index.min.mjs
+			distributable: source.min.mjs
+			distributable: index.d.mts
+
+*/
+
+/*
+
+	./dist/package/async/index.mjs
+
+
+*/
+
+import autogenerate from "fourtune"
+
+// (inside realm impl):
+import {autogenerate, target} from "fourtune/api"
+
+export const c3 = [
+	autogenerateSyncAsyncVariant(
+		"export/ScandirOptions<X>Type.d.mts",
+		"template/ScandirOptionsType.d.mts"
+	),
+
+	autogenerate("src/myFile", function() {
+		return "contents of my file"
+	}),
+
+	conditionalTarget({
+		condition() {
+
+		},
+
+		name: "package",
+
+		products: []
+	}),
+
+	preprocessFile(),
+
+	target({
+		name: "package",
+
+		products: [{
+			name: "async",
+			distributables: [{
+			//	id: "bundle"
+				file_name: "index.mjs",
+				generate() {
+					return "contents of index.mjs"
+				}
+			}]
+		}]
+	})
+]
+
+// ./dist/<product>/<distributable>
+
+export default {
+	realm: "js",
+	type: "package:async/sync",
+
+	target: {
+		function_name: "scandir"
+	},
+
+	autogenerate: {
+		"./src/": ""
+	}
+}
+
+
