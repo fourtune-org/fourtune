edef8e1379121ecdfe884513d232a77456ff1f79
eXory2024 <exory@anio.software>
2024-10-30 23:40:15 +0100

    Implement -init-project in a very crude way for now.

--- src/cli.mjs
+++ src/cli.mjs
@@ -2,5 +2,7 @@
 import {fourtune} from "./main.mjs"
 
 await fourtune(
-	process.argv[2]
+	process.argv[2], {
+		initialize_project: process.argv[3] === "-init-project"
+	}
 )
--- src/main.mjs
+++ src/main.mjs
@@ -2,19 +2,36 @@ import {stages} from "./stages/_index.mjs"
 import {createFourtuneSession} from "./lib/init/createFourtuneSession.mjs"
 import {initialize} from "./lib/session/initialize.mjs"
 import {runHooks} from "./lib/session/runHooks.mjs"
+import writeProjectInitFile from "./lib/writeProjectInitFile.mjs"
 
-export async function fourtune(project_root) {
+export async function fourtune(
+	project_root, {
+		initialize_project = false
+	} = {}
+) {
 	const session = await createFourtuneSession(process.argv[2])
 
 	await initialize(session)
 
 	process.chdir(project_root)
 
-	for (const {id, stage} of stages) {
-		console.log(id)
+	if (!initialize_project) {
+		for (const {id, stage} of stages) {
+			console.log(id)
 
-		await runHooks(session, `${id}.pre`)
-		await stage(session)
-		await runHooks(session, `${id}.post`)
+			await runHooks(session, `${id}.pre`)
+			await stage(session)
+			await runHooks(session, `${id}.post`)
+		}
+	} else {
+		if ("initializeProject" in session.realm) {
+			await session.realm.initializeProject(
+				session.public_interface, async (name, contents, {overwrite = false} = {}) => {
+					return await writeProjectInitFile(
+						session.public_interface, {name, contents}, overwrite
+					)
+				}
+			)
+		}
 	}
 }
