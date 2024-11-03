e20c86d9141c32335f65fad66c4e3753de3788ec
eXory2024 <exory@anio.software>
2024-11-04 00:10:21 +0100

    Add the ability for a product generator to create multiple files.

--- src/stages/5.createProducts.mjs
+++ src/stages/5.createProducts.mjs
@@ -9,16 +9,41 @@ export default {
 			console.log("> " + product_name)
 
 			for (const distributable of distributables) {
+				const base_path = path.join(
+					fourtune_session.project.root, "dist", product_name
+				)
+
 				const source = await distributable.generator(
 					fourtune_session.public_interface
 				)
 
-				await writeAtomicFile(
-					path.join(
-						fourtune_session.project.root, "dist",
-						product_name, distributable.file_name
-					), source, {create_parents: true}
-				)
+				if (Array.isArray(distributable.file_name)) {
+					if (!Array.isArray(source)) {
+						throw new Error(
+							`You must return an array from this generator (each element for every file to be written).`
+						)
+					}
+
+					if (distributable.file_name.length !== source.length) {
+						throw new Error(
+							`generator must return the same number of elements as files.`
+						)
+					}
+
+					for (let i = 0; i < distributable.file_name.length; ++i) {
+						await writeAtomicFile(
+							path.join(base_path, distributable.file_name[i]),
+							source[i],
+							{create_parents: true}
+						)
+					}
+				} else {
+					await writeAtomicFile(
+						path.join(base_path, distributable.file_name),
+						source,
+						{create_parents: true}
+					)
+				}
 			}
 		}
 	}
