ac30e3ae0ea4101d7e57749d628dfc796ed0dab7
eXory2024 <exory@anio.software>
2025-01-13 05:24:36 +0100

    Log before running hook.

--- src/lib/session/runHooks.mjs
+++ src/lib/session/runHooks.mjs
@@ -1,6 +1,8 @@
 export async function runHooks(
 	fourtune_session, hook_id
 ) {
+	console.log(`Running hook ${hook_id}`)
+
 	for (const hook of fourtune_session.hooks) {
 		if (hook.id === hook_id) {
 			await hook.fn(fourtune_session.public_interface)
